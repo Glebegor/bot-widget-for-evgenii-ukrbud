b56f30a4d932de4c05c6f937bd18fa05888e6c96
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-21 23:16:11 +0200

    Need to fix relation between service and controller. He routing it by db and service but need to be service and repository.

--- ringostat/Infrastructure/Data/ManagersRepository.py
+++ ringostat/Infrastructure/Data/ManagersRepository.py
@@ -5,3 +5,18 @@ def NewManagersRepository(db):
 class ManagersRepository:
     def __init__(self, db):
         self.db = db
+
+    def Get(self):
+        return 'QUERY GET'
+
+    def GetById(self, id):
+        return 'QUERY GET id'
+
+    def Create(self, data):
+        return 'QUERY CREATE'
+
+    def Update(self, id, data):
+        return 'QUERY UPDATE'
+
+    def Delete(self, id):
+        return 'QUERY DELETE'
